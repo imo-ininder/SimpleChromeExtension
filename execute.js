96d1aed00fd528a9f605c1bcefd972a396c66986
Imo Liu <tony840622@gmail.com>
2020-03-05 21:07:35 +0800

    Init

---
+++ execute.js
@@ -0,0 +1,5 @@
+const translatePrefix = "https://translate.google.com/translate?hl=zh-TW&sl=zh-CN&u=";
+
+// TODO : 從 Blocking page中抽出原本的網址。
+var targeSite = "https://komica.org/";
+document.location.href= translatePrefix + targeSite;
